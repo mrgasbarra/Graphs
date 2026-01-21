73ec4efa37cdffde2e9a300dc4ada82ec77169cd
Matthew Gasbarra <mrgasbarra85@gmail.com>
2026-01-20 20:48:44 -0800

    completed exercise

--- graph.js
+++ graph.js
@@ -11,25 +11,79 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
-
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
+  
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    for (let adjacent of vertex.adjacent) {
+      adjacent.adjacent.delete(vertex);
+    }
+    this.nodes.delete(vertex);
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const visited = new Set();
+    const result = [];
+
+    function dfs(vertex) {
+      if (!vertex) return;
+      visited.add(vertex);
+      result.push(vertex.value);
+      
+      for (let neighbor of vertex.adjacent) {
+        if (!visited.has(neighbor)) {
+          dfs(neighbor);
+        }
+      }
+    }
+
+    dfs(start);
+    return result;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    const visited = new Set();
+    const result = [];
+    const queue = [start];
+
+    while (queue.length) {
+      const current = queue.shift();
+      if (!visited.has(current)) {
+        visited.add(current);
+        result.push(current.value);
+        for (let neighbor of current.adjacent) {
+          if (!visited.has(neighbor)) {
+            queue.push(neighbor);
+          }
+        }
+      }
+    }
+
+    return result;
+  }
 }
 
 module.exports = {Graph, Node}
